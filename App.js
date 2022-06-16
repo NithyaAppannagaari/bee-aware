f6b495fc67018cfb807639af51e6ce123d33693e
tanishakholiya2 <tanishakholiya@gmail.com>
2022-06-15 23:31:10 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>BEE AWARE</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a2e0ae',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
